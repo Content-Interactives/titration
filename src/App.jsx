1ffbd464945e5ae9d35390c092b88d19ed80ba69
User <user@example.com>
2025-04-23 11:47:45 -0700

    added app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Titration from './components/Titration'
+
+
+function App() {
+  return (
+      <div className="App">
+        <Titration />
+      </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/Titration.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+
+const Titration = () => {
+
+
+	return (
+        <div></div>
+)
+};
+
+
+export default Titration;
